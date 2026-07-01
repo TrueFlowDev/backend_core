fb6dc5b74fa944745a8415410135e4fb1c3f0d74
Ali127Dev <Ali127Dev@gmail.com>
2026-07-02 00:04:27 +0330

    chore: init notification module

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/TrueFlowDev/Backend/internal/module/auth"
+	"github.com/TrueFlowDev/Backend/internal/module/notification"
 	"github.com/TrueFlowDev/Backend/internal/module/user"
 	"github.com/TrueFlowDev/Backend/internal/shared"
 	"go.uber.org/fx"
@@ -13,6 +14,7 @@ func main() {
 			shared.Module,
 			user.Module,
 			auth.Module,
+			notification.Module,
 		).
 		Run()
 }
