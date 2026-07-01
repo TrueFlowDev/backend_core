da58f63a8b8c78014b7b7c326fe3f191fbccebb8
Ali127Dev <Ali127Dev@gmail.com>
2026-07-01 15:54:50 +0330

    feat: validate hashed password to not be empty

--- internal/module/user/domain/value_object/password.go
+++ internal/module/user/domain/value_object/password.go
@@ -1,11 +1,26 @@
 package value_object
 
+import (
+	"errors"
+	"strings"
+)
+
+var (
+	ErrHashedPasswordRequired = errors.New("hashed password is required")
+)
+
 type HashedPassword struct {
 	value string
 }
 
-func NewHashedPassword(hash string) HashedPassword {
-	return HashedPassword{value: hash}
+func NewHashedPassword(hash string) (HashedPassword, error) {
+	hash = strings.TrimSpace(hash)
+
+	if hash == "" {
+		return HashedPassword{}, ErrHashedPasswordRequired
+	}
+
+	return HashedPassword{value: hash}, nil
 }
 
 func (p HashedPassword) Value() string { return p.value }
