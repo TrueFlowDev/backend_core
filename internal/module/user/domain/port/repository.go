0a49fbddf90dbe8aafe09dfff279e37fff3e12de
Ali127Dev <Ali127Dev@gmail.com>
2026-07-01 16:41:32 +0330

    feat: add user repository

---
+++ internal/module/user/domain/port/repository.go
@@ -0,0 +1,15 @@
+package port
+
+import (
+	"github.com/TrueFlowDev/Backend/internal/module/user/domain/entity"
+	user "github.com/TrueFlowDev/Backend/internal/module/user/domain/value_object"
+	shared "github.com/TrueFlowDev/Backend/internal/shared/domain/value_object"
+)
+
+type UserRepository interface {
+	Create(user *entity.User) error
+
+	FindByID(id user.UserID) (*entity.User, error)
+	FindByPhone(phone shared.Phone) (*entity.User, error)
+	ExistsByPhone(phone shared.Phone) (bool, error)
+}
