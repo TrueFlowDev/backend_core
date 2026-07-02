acca84e59d9ac58e0441bcd2625f1daec47b7ae1
Ali127Dev <Ali127Dev@gmail.com>
2026-07-02 20:58:01 +0330

    feat: add create user mig file

---
+++ migrations/20260702165026_create_user.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS users
+(
+    id         UUID PRIMARY KEY,
+
+    phone      VARCHAR(20) NOT NULL,
+    password   VARCHAR(255),
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS uq_users_phone ON users(phone) WHERE deleted_at IS NULL;
+
+-- +goose Down
+DROP INDEX IF EXISTS uq_users_phone;
+DROP TABLE IF EXISTS users;
