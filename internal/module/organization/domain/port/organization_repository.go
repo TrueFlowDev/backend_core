f67dbb5dfe03070a57b63b06eb2141797b6fb31e
Ali127Dev <Ali127Dev@gmail.com>
2026-07-08 18:44:58 +0330

    feat: add organization repository

---
+++ internal/module/organization/domain/port/organization_repository.go
@@ -0,0 +1,20 @@
+package port
+
+import (
+	"context"
+
+	"github.com/Ali127Dev/xerr"
+	"github.com/TrueFlowDev/Backend/internal/module/organization/domain/entity"
+	"github.com/TrueFlowDev/Backend/internal/module/organization/domain/value_object"
+)
+
+var (
+	ErrOrganizationNotFound   = xerr.New(xerr.CodeRecordNotFound, xerr.WithMeta("organization", xerr.ErrorReasonNotFound))
+	ErrOrganizationRepository = xerr.New(xerr.CodeDatabaseError)
+)
+
+type OrganizationRepository interface {
+	Save(ctx context.Context, organization *entity.Organization) error
+
+	FindByID(ctx context.Context, id value_object.OrganizationID) (*entity.Organization, error)
+}
