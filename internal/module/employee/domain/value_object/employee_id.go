fca78eb94f00f27fe23c9ba4a7025fcc84b9adaa
Ali127Dev <Ali127Dev@gmail.com>
2026-07-11 23:00:48 +0330

    feat: add employee id

---
+++ internal/module/employee/domain/value_object/employee_id.go
@@ -0,0 +1,14 @@
+package value_object
+
+import "strings"
+
+type EmployeeID struct {
+	value string
+}
+
+func NewEmployeeID(id string) EmployeeID {
+	id = strings.TrimSpace(id)
+	return EmployeeID{value: id}
+}
+
+func (u EmployeeID) Value() string { return u.value }
