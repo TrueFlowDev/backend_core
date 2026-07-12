c6012d10dd9761b0a2e0a12cef5233a933d73625
Ali127Dev <Ali127Dev@gmail.com>
2026-07-12 13:20:57 +0330

    chore: disable swagger in production

--- internal/platform/server/http/swagger.go
+++ internal/platform/server/http/swagger.go
@@ -2,12 +2,15 @@ package http
 
 import (
 	_ "github.com/TrueFlowDev/Backend/docs/swagger"
+	"github.com/TrueFlowDev/Backend/internal/platform/config"
 
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func RegisterSwagger(r *gin.Engine) {
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+func RegisterSwagger(r *gin.Engine, cfg *config.Config) {
+	if cfg.App.Mode == "dev" {
+		r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
 }
